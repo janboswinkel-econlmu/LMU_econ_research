4b11e08560f4f4ce0958ffb1c58ab9d67baae2af
Boswinkel <jan.boswinkel@vwl.lmu.de>
2025-10-02 12:34:01 +0200

    First cleanup

---
+++ google_functions.py
@@ -0,0 +1,63 @@
+#region libraries
+import requests
+
+#endregion
+########################################################################################################################
+
+#region google
+
+def google_ocr(image_path):
+    """Performs Optical Character Recognition (OCR) on an image by invoking the Vertex AI REST API.
+        Returns array with columns: index, detected_word, x1, y1, x2, y2
+    """
+
+    # Securely fetch the API key from environment variables
+    api_key = os.environ.get("GOOGLE_API_KEY")
+    if not api_key:
+        raise ValueError("GCP_API_KEY environment variable must be defined.")
+
+    # Construct the Vision API endpoint URL
+    vision_api_url = f"https://vision.googleapis.com/v1/images:annotate?key={api_key}"
+
+    # Read and encode the local image
+    with open(image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode()
+
+    # Define the request payload for text detection
+    request_payload = {
+        "requests": [
+            {
+                "image": {
+                        "content": encoded_image
+                },
+                "features": [
+                    {
+                        "type": "TEXT_DETECTION"
+                    }
+                ]
+            }
+        ]
+    }
+
+    # Send a POST request to the Vision API
+    response = requests.post(vision_api_url, json=request_payload)
+    response.raise_for_status()  # Check for HTTP errors
+
+    response_json = response.json()
+
+    rows=[]
+    for item in response_json['responses'][0]['textAnnotations'][1:]:
+        vertices=item['boundingPoly']['vertices']
+        all_x, all_y = [], []
+        for vertex in vertices:
+            all_x.append(vertex.get('x', 0))
+            all_y.append(vertex.get('y', 0))
+        x1, x2, y1, y2 = min(all_x), max(all_x), min(all_y), max(all_y)
+        row=np.array([item['description'], x1, y1, x2, y2], dtype=object)
+        rows.append(row)
+    rows=np.vstack(rows)
+    rows=np.hstack((np.arange(len(rows)).reshape(-1,1), rows))
+
+    return rows
+#endregion
+########################################################################################################################
